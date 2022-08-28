6e29114f74c1b41ab200060d714d528880b5852e
MariaEueniaGR <gonzalezrangel.me@gmail.com>
2022-08-28 16:20:37 -0300

    URL heruku

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-URL = 'http://localhost:8080/educacion/';
+URL = 'https://maru-backend.herokuapp.com/educacion/';
  
 constructor(private httpClient : HttpClient) {}
 
