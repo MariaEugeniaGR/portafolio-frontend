3b1996a82b2d9040ca96513b8a534939141da7d1
francojgr18 <frgonzalez23@gmail.com>
2022-08-29 20:44:52 -0300

    otro intento :(

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const path = require('path');
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const app = express();
 // Serve static files
-app.use(express.static(__dirname + '/dist/maru-frontend'));
+app.use(express.static(__dirname + '/dist/portafolio'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/maru-frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/portafolio/index.html'));
 });
 
 // default Heroku port
